59445bf06854b92d16d9c3819cf8db020350b8ce
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2016-01-27 17:15:47 +0600

    test fix

--- app/src/androidTest/java/com/viorsan/mergeadapterdemo/ApplicationTest.java
+++ app/src/androidTest/java/com/viorsan/mergeadapterdemo/ApplicationTest.java
@@ -15,31 +15,5 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         super(Application.class);
     }
 
-    @Test
-    public void shouldDateTypeBefore() {
-        // для этого теста контекст нужен хотя вроде бы обычный юнит текст так что...
-        DATE_TYPE dt=DATE_TYPE.BEFORE;
-        assertEquals(dt.getStringValue(getContext()), getContext().getString(R.string.before));
-    }
-
-    @Test
-    public void shouldDateTypeToday() {
-        // для этого теста контекст нужен хотя вроде бы обычный юнит текст так что...
-        DATE_TYPE dt=DATE_TYPE.TODAY;
-        assertEquals(dt.getStringValue(getContext()),getContext().getString(R.string.today));
-    }
-
-    @Test
-    public void shouldDateTypeTwoDaysAgo() {
-        // для этого теста контекст нужен хотя вроде бы обычный юнит текст так что...
-        DATE_TYPE dt=DATE_TYPE.TWO_DAYS_AGO;
-        assertEquals(dt.getStringValue(getContext()),getContext().getString(R.string.twodaysago));
-    }
-    @Test
-    public void shouldDateTypeThreeDaysAgo() {
-        // для этого теста контекст нужен хотя вроде бы обычный юнит текст так что...
-        DATE_TYPE dt=DATE_TYPE.THREE_DAYS_AGO;
-        assertEquals(dt.getStringValue(getContext()),getContext().getString(R.string.threedaysago));
-    }
 
 }
