dcc90fd3d00d43ef8387c6e289abcd35f419977d
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2016-01-27 14:29:19 +0600

    initial tests using onData

--- app/src/androidTest/java/com/viorsan/mergeadapterdemo/MainActivityTest.java
+++ app/src/androidTest/java/com/viorsan/mergeadapterdemo/MainActivityTest.java
@@ -5,13 +5,17 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
 
 /**
  * Created by Dmitriy Kazimirov, e-mail:dmitriy.kazimirov@viorsan.com on 27.01.16.
  */
+
 
 public class MainActivityTest {
 
@@ -20,7 +24,8 @@ public class MainActivityTest {
             new ActivityTestRule<>(MainActivity.class);
 
     /**
-     * Есть контрол с  'Today' строкой
+     * Есть контрол с  'Today' - локализуемым ресурсом
+     * Не работает если контрол не на экране (а в случае списка так может быть запросто)
      */
     @Test
     public void shouldHaveToday() {
@@ -29,32 +34,29 @@ public class MainActivityTest {
 
     }
 
-
     /**
      * Есть контрол с 'Before' - локализуемым ресурсом
      */
     @Test
     public void shouldHaveBefore() {
         // then
-        onView(withText(R.string.before)).check(matches(isDisplayed()));
+        onData(allOf(withText(R.string.before))).check(matches(isDisplayed()));
     }
-
     @Test
     public void shouldHaveYesterday() {
         // then
-        onView(withText(R.string.yesterday)).check(matches(isDisplayed()));
+        onData(allOf(withText(R.string.yesterday))).check(matches(isDisplayed()));
     }
     @Test
     public void shouldHaveTwoDaysAgo() {
         // then
-        onView(withText(R.string.twodaysago)).check(matches(isDisplayed()));
+        onData(allOf(withText(R.string.twodaysago))).check(matches(isDisplayed()));
     }
     @Test
     public void shouldHaveThreeDaysAgo() {
         // then
-        onView(withText(R.string.threedaysago)).check(matches(isDisplayed()));
+        onData(allOf(withText(R.string.threedaysago))).check(matches(isDisplayed()));
     }
-
 
 
 }
